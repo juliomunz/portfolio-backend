66e405729d0d354aa10cf851562805ed9f34c3a7
Julio Muñoz <juliomunoz@MacBook-Air-de-Julio.local>
2026-02-12 01:35:30 -0300

    change: nodemailer config

--- server.js
+++ server.js
@@ -41,7 +41,9 @@ const contactLimiter = rateLimit({
 
 // Configurar Email
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
